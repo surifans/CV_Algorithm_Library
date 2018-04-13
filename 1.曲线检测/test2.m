e8fbc82cabee068be8cbe86bf0483e41391faa96
dingjunjie <djj1@meitu.com>
2018-04-13 16:16:58 +0800

    【优化】添加大小图块判断，大于20*20的图块判断最大宽像素占图像宽的比例来排除非线条像素块。小于20*20的像素块，将其扩宽，判断增加20pad的范围内是否有像素将该块排除。

--- 1.曲线检测/test2.m
+++ 1.曲线检测/test2.m
@@ -1,7 +1,7 @@
     clear;
     clc;
     close all;
-    for i=1:10
+    for i=2%1:10
         I  = rgb2gray(imread(['1 (',num2str(i),').jpg']));
         thresh = graythresh(I);     %自动确定二值化阈值
         bw_img = im2bw(I,thresh*0.6);       %对图像二值化
@@ -17,7 +17,7 @@
 %         figure(2);
 %         imshow(bw_img);
         
-        
+        %imwrite(bw_img, [num2str(i),'.jpg']); 
         
         %————区域标记————
         %——获取联通区域
@@ -50,14 +50,14 @@
         %x=regionprops(bw_img, 'PixelIdxList');  
         YX=regionprops(bw_img, 'PixelList');  
         
-        for i1=1:size(Img,1)
+        for i1=1:size(Img,1)    %遍历每个图块
             subImg=Img(i1).Image;
             %imshow(subImg); 
             
             [w,h]=size(subImg);
-            
-            if w>20||h>20
-                XY=YX(i1).PixelList;
+            XY=YX(i1).PixelList;
+            if w>20||h>20 %考虑图块比较大的情形下
+                
                 
                 sumx=sum(subImg,1);
                 sumy=sum(subImg,2);
@@ -78,15 +78,34 @@
                         bw_img(XY(i2,2),XY(i2,1))=0;
                     end
                 end
+            else   %考虑图块比较小的情形
+                max_rect = rects(i1, :);%前两个值为左上点的坐标，后面两值为方框的大小
+                wfloor=floor(max_rect(2));%行
+                hfloor=floor(max_rect(1));%列
+                
+                N1=20;
+                wtop=max(wfloor-N1,1);
+                hleft=max(hfloor-N1,1);
+                
+                wdown=min(wfloor+max_rect(4)+N1,w);
+                hright=min(hfloor+max_rect(3)+N1,h);
+                
+                increas1=double(bw_img(wtop:wdown,hleft:hfloor));
+                increas2=double(bw_img(wtop:wdown,hfloor+max_rect(3):hright));
+                increas3=double(bw_img(wtop:wfloor,hleft:hright));
+                increas4=double(bw_img(wfloor+max_rect(4):wdown,hleft:hright));
+                
+                Increased=sum(sum(increas1))+sum(sum(increas2))+sum(sum(increas3))+sum(sum(increas4));
+                
+                if Increased==0;
+                    for i2=1:size(XY)
+                        bw_img(XY(i2,2),XY(i2,1))=0;
+                    end
+                end
             end
             
             
-%             max_rect = rects(i, :);%前两个值为左上点的坐标，后面两值为方框的大小
-%             figure(2);   
-%             imshow(bw_img);  
-%             hold on;
-%             rectangle('position', max_rect, 'EdgeColor', 'r');  
-            
+
             
             
             %figure(3);  
@@ -94,127 +113,8 @@
             
         end
         %imshow(bw_img);
+        imwrite(bw_img, [num2str(i),'2.jpg']); 
         
-        imwrite(bw_img, [num2str(i),'.jpg']); 
         
     end
-    
-    
-%     I2=double(I2);
-%     [w,h]=size(I2);
-    
-    
-    
-%     [L, num] = bwlabel(I2, 8);     
-%     status = regionprops(L,'Area', 'Perimeter');
-%     area = [status.Area];
-    
-    
-    
-%     markarea=zeros(w,h);
-%     %————把每个像素周围的8个点是否有像素值存入表中;
-%     n=1;
-%     for i=2:w-2
-%         for j=2:h-2
-%             l=[];
-% %             Imark1=markarea;
-%             mask=zeros(w,h);
-%             if I2(i,j)>0&&markarea(i,j)==0;%确定这个点没有被遍历过，且有像素值
-%                 markarea(i,j)=1;%当前这个点要被记录下，避免重复遍历
-%                 l(1,:)=Calcul_around(i,j,I2);%从第一个点开始，l记录其周围的点是否有像素，8个方向，有的话记为1,,
-%                 
-%                 n=1;
-%                 r(n,:)=[i j];
-%                 while(size(l,1)>0)
-%                    
-%                    %每个循环中先查看第一行，把新的点的信息添加到最后一行
-%                    if l(1,1)>0&&markarea(l(1,9)-1,l(1,10)-1)==0  %左上角    有像素值，且未被遍历到的时候
-%                        l(1,1)=0;                                 %        
-%                        l(size(l,1)+1,:)=Calcul_around(l(1,9)-1,l(1,10)-1,I2);
-%                        l(size(l,1),8)=0;
-%                        markarea(l(1,9)-1,l(1,10)-1)=1;
-%                    end
-%                    if l(1,2)>0&&markarea(l(1,9)-1,l(1,10))==0  %上
-%                        l(1,2)=0; 
-%                        l(size(l,1)+1,:)=Calcul_around(l(1,9)-1,l(1,10),I2);
-%                        l(size(l,1),7)=0;
-%                        markarea(l(1,9)-1,l(1,10))=1;
-%                    end
-%                    if l(1,3)>0&&markarea(l(1,9)-1,l(1,10)+1)==0  %右上
-%                        l(1,3)=0; 
-%                        l(size(l,1)+1,:)=Calcul_around(l(1,9)-1,l(1,10)+1,I2);
-%                        l(size(l,1),6)=0;
-%                        markarea(l(1,9)-1,l(1,10)+1)=1;
-%                    end
-%                    if l(1,4)>0&&markarea(l(1,9),l(1,10)-1)==0  %左
-%                        l(1,4)=0; 
-%                        l(size(l,1)+1,:)=Calcul_around(l(1,9),l(1,10)-1,I2);
-%                        l(size(l,1),5)=0;
-%                        markarea(l(1,9),l(1,10)-1)=1;
-%                    end
-%                    if l(1,5)>0&&markarea(l(1,9),l(1,10)+1)==0  %右
-%                        l(1,5)=0; 
-%                        l(size(l,1)+1,:)=Calcul_around(l(1,9),l(1,10)+1,I2);
-%                        l(size(l,1),4)=0;
-%                        markarea(l(1,9),l(1,10)+1)=1;
-%                    end
-%                    if l(1,6)>0&&markarea(l(1,9)+1,l(1,10)-1)==0  %左下
-%                        l(1,6)=0; 
-%                        l(size(l,1)+1,:)=Calcul_around(l(1,9)+1,l(1,10)-1,I2);
-%                        l(size(l,1),3)=0;
-%                        markarea(l(1,9)+1,l(1,10)-1)=1;
-%                    end
-%                    if l(1,7)>0&&markarea(l(1,9)+1,l(1,10))==0  %下
-%                        l(1,7)=0; 
-%                        l(size(l,1)+1,:)=Calcul_around(l(1,9)+1,l(1,10),I2);
-%                        l(size(l,1),2)=0;
-%                        markarea(l(1,9)+1,l(1,10))=1;
-%                    end
-%                    if l(1,8)>0&&markarea(l(1,9)+1,l(1,10)+1)==0  %右下
-%                        l(1,8)=0; 
-%                        l(size(l,1)+1,:)=Calcul_around(l(1,9)+1,l(1,10)+1,I2);
-%                        l(size(l,1),1)=0;
-%                        markarea(l(1,9)+1,l(1,10)+1)=1;
-%                    end
-%                    %l(1:1-1,:)=l(1:1-1,:);
-%                    if size(l,1)==1;
-%                        r(n,:)=[l(9) l(10)];
-%                        n=n+1;
-%                        l=[];
-%                        
-%                    else
-%                        r(n,:)=[l(size(l,1),9) l(size(l,1),10)];%每次删一个点的时候，把这个点记录下来
-%                        n=n+1;
-%                        l(1:size(l,1)-1,:)=l(2:size(l,1),:);
-%                        l(size(l,1),:)=[];
-%                        
-%                    end
-%                    for i2=1:size(r,1)
-%                         mask(r(i2,1),r(i2,2))=255;
-%                    end
-%                    figure(2);
-%                    imshow(mask);
-%                 end
-% %                 minrx=min(r(:,1))-1;
-% %                 minry=min(r(:,2))-1;
-% %                 
-% %                 r(:,1)=r(:,1)-minrx;
-% %                 r(:,2)=r(:,2)-minry;
-% %                 
-% %                 maxr=max(max(r));
-% %                 mask=zeros(maxr,maxr);
-%                 
-%                 
-%                 
-%             end
-% %             Ires=markarea-Imark1;
-% %             if sum(Ires)>0
-% %                 imshow(Ires);
-% %             end
-%         end
-%     end
-% 
-%     
-%     markarea = im2bw(markarea,0);       %对图像二值化
-%     figure(2);
-%     imshow(markarea);
+ 
